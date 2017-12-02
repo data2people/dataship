629ae248931e49908e22ccdbdb34f6447226fe4f
RAN3000 <giacomoran@gmail.com>
2017-12-02 20:11:05 +0100

    add manage accounts page

---
+++ public/handle-logins.js
@@ -0,0 +1,9 @@
+function reqPermission(id) {
+    enabled = (document.getElementById(id).getElementsByClassName("btn")[0].textContent == "Enable");
+    if (enabled) {
+        // post request
+        document.getElementById(id).getElementsByClassName("btn")[0].textContent = "Disable";
+    } else {
+        document.getElementById(id).getElementsByClassName("btn")[0].textContent = "Enable";
+    }
+}
