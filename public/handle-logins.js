5c960d4868806663825ee1eb7b70df710127149c
RAN3000 <giacomoran@gmail.com>
2017-12-02 20:37:17 +0100

    update providers state on start

--- public/handle-logins.js
+++ public/handle-logins.js
@@ -8,6 +8,44 @@ function reqPermission(id) {
     }
 }
 
+function updateProviderStates(providerState, targetClass) {
+    var prov = providerState["providers"];
+    var provsLength = prov.length;
+    for (var i = 0; i < provsLength; i++) {
+        if (prov[i] != null) {
+            if (prov[i]["enabled"]) {
+                // post request
+                document.getElementById(prov[i]["name"]).getElementsByClassName(targetClass)[0].textContent = "Enable";
+            } else {
+                document.getElementById(prov[i]["name"]).getElementsByClassName(targetClass)[0].textContent = "Disable";
+            }
+        }
+    }
+}
+
 function getPermissions() {
     // return dumb json
+    dumb = 
+    {
+        "providers": [
+        {
+                        "name": "facebook",
+                        "enabled": false
+                                                
+        },
+        {
+                        "name": "google-plus",
+                        "enabled": true
+                                                
+        },
+        {
+                        "name": "google-fit",
+                        "enabled": false
+                                                
+        }
+            
+        ]
+
+    };
+    return dumb;
 }
