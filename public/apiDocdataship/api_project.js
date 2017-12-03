f2843cfafcde7cf7956296a280fbdaa43ba87219
RAN3000 <giacomoran@gmail.com>
2017-12-03 07:34:08 +0100

    add documentation

---
+++ public/apiDocdataship/api_project.js
@@ -0,0 +1,13 @@
+define({
+  "name": "example-basic",
+  "version": "0.1.0",
+  "description": "apiDoc basic example",
+  "template": {
+    "withCompare": false
+  },
+  "generator": {
+    "version": "0.9.0",
+    "time": "2014-11-28T14:54:55.224Z"
+  },
+  "apidoc": "0.2.0"
+});
